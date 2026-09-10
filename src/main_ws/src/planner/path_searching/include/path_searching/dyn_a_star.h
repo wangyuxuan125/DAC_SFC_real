f6832bbad563a49b0775a8438471fc64b1040a01
wangyuxuan125 <162686852+wangyuxuan125@users.noreply.github.com>
2026-09-10 23:34:59 +0800

    Add optional six-connected A* search

--- src/main_ws/src/planner/path_searching/include/path_searching/dyn_a_star.h
+++ src/main_ws/src/planner/path_searching/include/path_searching/dyn_a_star.h
@@ -91,7 +91,7 @@ public:
 
 	void initGridMap(GridMap::Ptr occ_map, const Eigen::Vector3i pool_size);
 
-	ASTAR_RET AstarSearch(const double step_size, Eigen::Vector3d start_pt, Eigen::Vector3d end_pt);
+	ASTAR_RET AstarSearch(const double step_size, Eigen::Vector3d start_pt, Eigen::Vector3d end_pt, bool use_six_connected = false);
 
 	std::vector<Eigen::Vector3d> getPath();
 };
